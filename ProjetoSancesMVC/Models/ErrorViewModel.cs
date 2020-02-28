6cc81217025debb34b03a7a23bc8af88635a0e88
Giovani Candido <Giiiozito@gmail.com>
2020-02-28 10:51:27 -0300

    Project Created

---
+++ ProjetoSancesMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoSancesMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
